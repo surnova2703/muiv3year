801cfa75bed45f7d93ea5b44475c1e4b6421a2c7
Варвара Сурнова <surnova2703@gmail.com>
2026-05-04 15:41:07 +0300

    Добавлены данные диплома

---
+++ pg_config.py
@@ -0,0 +1,8 @@
+PG_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "dbname": "openalex_db",
+    "user": "postgres",
+    "password": "Koshka123",  # замените на свой пароль
+    "client_encoding": "utf8",
+}
